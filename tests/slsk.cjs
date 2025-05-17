9859ab57e7de1e42d7f08105980b3d18a9ab6031
Trent <60309933+luphoria@users.noreply.github.com>
2025-05-17 03:20:32 -0500

    fix livelook test (the library is broken)

--- tests/slsk.cjs
+++ tests/slsk.cjs
@@ -1,6 +1,7 @@
 const _ENV = import("../.env.js");
 const LiveLook = require('livelook');
 const fs = require('fs');
+const sanitize = require("sanitize-filename");
 
 const init = async () => {
   const ENV = await _ENV; // ??????????
@@ -9,7 +10,8 @@ const init = async () => {
   let livelook = new LiveLook({
       username: ENV.slskAccounts[0].username,
       password: ENV.slskAccounts[0].password,
-      sharedFolder: "./mp3s"
+      sharedFolder: "./tests/slsk-downloads", 
+      waitPort: 2929
   });
   
   livelook.on('error', console.error);
@@ -27,22 +29,21 @@ const init = async () => {
       //     console.log(`<${msg.username}> ${msg.message}`);
       // });
   
-      livelook.search('oscar jerome', (err, res) => {
+      livelook.searchFiles('oscar jerome channel your anger', (err, res) => {
           if (err) {
               return console.error(err);
           }
   
           res = res.filter(item => item.slotsFree);
   
-          console.log(res);
-  
           if (!res) {
               console.log('no files found :(');
               return;
           }
   
-          let downloaded = fs.createWriteStream('file.mp3');
           res = res.sort((a, b) => a.speed > b.speed ? -1: 0)[0];
+          console.log(res);
+          let downloaded = fs.createWriteStream(`./tests/slsk-downloads/${sanitize(res.file)}`);
           livelook.downloadFile(res.username, res.file).pipe(downloaded);
       });
   });
