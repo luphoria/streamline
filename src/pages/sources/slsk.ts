e65e89c5d2c094dfecdd1af3ded19bf969b9fde5
Percs <me@percs.dev>
2025-05-18 23:40:09 -0500

    feat: switch to /api/source and clean up slsk fetching

--- src/pages/sources/slsk.ts
+++ src/pages/sources/slsk.ts
@@ -7,8 +7,7 @@ import { Readable } from "stream";
 
 // Create a search in slskd
 const CreateSearch = async (query) => {
-	let data;
-	await fetch(`${slskd.url}/api/v0/searches`, {
+	const response = await fetch(`${slskd.url}/api/v0/searches`, {
 		method: "POST",
 		body: JSON.stringify({
 			searchText: query,
@@ -19,13 +18,7 @@ const CreateSearch = async (query) => {
 			"X-API-Key": slskd.apikey,
 		},
 	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((res) => {
-			data = res;
-			console.log(res);
-		});
+	const data = await response.json();
 
 	return {
 		query: data.searchText,
@@ -35,10 +28,9 @@ const CreateSearch = async (query) => {
 
 // Check that search status
 const AwaitSearchCompletion = async (id) => {
-	let isComplete = false;
-	while (!isComplete) {
+	while (true) {
 		console.log("Awaiting search completion . . .");
-		await fetch(`${slskd.url}/api/v0/searches/${id}`, {
+		const response = await fetch(`${slskd.url}/api/v0/searches/${id}`, {
 			method: "GET",
 			headers: {
 				Accept: "application/json",
@@ -46,36 +38,25 @@ const AwaitSearchCompletion = async (id) => {
 				"X-API-Key": slskd.apikey,
 			},
 		})
-			.then((res) => {
-				return res.json();
-			})
-			.then((res) => {
-				console.log(res);
-				if (res.isComplete) {
-					isComplete = true;
-
-					return res;
-				}
-			});
-		if (isComplete) {
-			console.log("== Search Completed ==");
-			await SearchResponses(id);
-
-			return {
-				id: id,
-				isComplete: true,
-			};
+		const data = await response.json();
+		if (data.isComplete) {
+			break;
 		}
-
 		// repeat until finished
 		await new Promise((resolve) => setTimeout(resolve, 2000));
 	}
+	console.log("== Search Completed ==");
+	await SearchResponses(id);
+
+	return {
+		id: id,
+		isComplete: true,
+	};
 };
 
 // Retrieve search responses
 const SearchResponses = async (id) => {
-	let data;
-	await fetch(`${slskd.url}/api/v0/searches/${id}/responses`, {
+	const response = await fetch(`${slskd.url}/api/v0/searches/${id}/responses`, {
 		method: "GET",
 		headers: {
 			Accept: "application/json",
@@ -83,22 +64,14 @@ const SearchResponses = async (id) => {
 			"X-API-Key": slskd.apikey,
 		},
 	})
-		.then((res) => {
-			return res.json();
-		})
-		.then((res) => {
-			console.log(res);
-			data = res;
-
-			return res;
-		});
-
+	const data = await response.json();
+	console.log(data);
+	
 	return data;
 };
 
 const CreateDownload = async (username, filePath, size) => {
-	let data;
-	await fetch(`${slskd.url}/api/v0/transfers/downloads/${username}`, {
+	const response = await fetch(`${slskd.url}/api/v0/transfers/downloads/${username}`, {
 		method: "POST",
 		// TODO: it looks like we can request multiple downloads at once. We should probably try the first few results to avoid failure.
 		body: JSON.stringify([
@@ -112,18 +85,13 @@ const CreateDownload = async (username, filePath, size) => {
 			"Content-Type": "application/json",
 			"X-API-Key": slskd.apikey,
 		},
-	}).then((res) => {
-		data = res;
-		return res;
-	});
-
-	return data;
+	})
+	
+	return response;
 };
 
 const AwaitDownloadCompletion = async (username, filePath) => {
-	// First we need to get the ID of the download that we want (the download creation api does not return this value)
-	let fileId;
-	await fetch(`${slskd.url}/api/v0/transfers/downloads/${username}`, {
+	const response = await fetch(`${slskd.url}/api/v0/transfers/downloads/${username}`, {
 		method: "GET",
 		headers: {
 			Accept: "application/json",
@@ -131,24 +99,19 @@ const AwaitDownloadCompletion = async (username, filePath) => {
 			"X-API-Key": slskd.apikey,
 		},
 	})
-		.then((res) => {
-			console.log(res);
-			return res.json();
-		})
-		.then((res) => {
-			let file = res.directories[0].files.filter((file) => {
-				return file.filename == filePath;
-			});
-			console.log("========");
-			console.log(file);
-			fileId = file[0].id;
-			console.log(fileId);
-		});
+	const data = await response.json();
+	const file = data.directories[0].files.filter((file) => {
+		return file.filename == filePath;
+	});
+	console.log("========");
+	console.log(file);
+	// First we need to get the ID of the download that we want (the download creation api does not return this value)
+	const fileId = file[0].id;
+	console.log(fileId);
 	// Repeatedly check to see if the file is at 100% yet
-	let isComplete = false;
-	while (!isComplete) {
+	while (true) {
 		console.log("Awaiting download completion . . .");
-		await fetch(
+		const response = await fetch(
 			`${slskd.url}/api/v0/transfers/downloads/${username}/${fileId}`,
 			{
 				method: "GET",
@@ -159,33 +122,25 @@ const AwaitDownloadCompletion = async (username, filePath) => {
 				},
 			}
 		)
-			.then((res) => {
-				console.log(res);
-				return res.json();
-			})
-			.then((res) => {
-				console.log(res);
-				if (res.percentComplete >= 100) {
-					isComplete = true;
-					return res;
-				}
-			});
-		if (isComplete) {
-			console.log("== Download Completed ==");
-			return {
-				username: username,
-				id: fileId,
-				filepath: filePath,
-				isComplete: true,
-			};
+		const data = await response.json();
+		if (data.percentComplete >= 100) {
+			break;
 		}
-
 		// repeat until finished
 		await new Promise((resolve) => setTimeout(resolve, 2000));
 	}
+	console.log("== Download Completed ==");
+
+	return {
+		username: username,
+		id: fileId,
+		filepath: filePath,
+		isComplete: true,
+	};
 };
 
-export async function GET({ params, request }) {
+export default async function (query) {
+	/*
 	const url = new URL(request.url);
 
 	const searchParams = new URLSearchParams(url.search);
@@ -194,9 +149,8 @@ export async function GET({ params, request }) {
 	// Maybe only take MBID and then query it server-side to get the %ARTIST - %TITLE plus validate the mbid?
 	const query = decodeURIComponent(searchParams.get("query"));
 	const mbid = decodeURIComponent(searchParams.get("mbid"));
-
+	*/
 	const search = await CreateSearch(query);
-
 	await AwaitSearchCompletion(search.id);
 	let searchRes = await SearchResponses(search.id);
 	// Sort by upload speed -- we will want more options later, like preferring flac, etc
@@ -204,12 +158,12 @@ export async function GET({ params, request }) {
 	// Some responses are for locked-only files (TODO: add a check for if we have access to locked files maybe? idk how they work)
 	searchRes = searchRes.filter(response => {return response.files.length > 0});
 
-	let chosenSearchRes = searchRes[0];
-	let chosenFile = chosenSearchRes.files[0];
-
+	const chosenSearchRes = searchRes[0];
 	console.log(searchRes[0]);
+	const chosenFile = chosenSearchRes.files[0];
+
 	// Possibly use an array of these options to do multiple downloads in one request ?
-	let download = await CreateDownload(
+	const download = await CreateDownload(
 		chosenSearchRes.username,
 		chosenFile.filename,
 		chosenFile.size
@@ -218,17 +172,18 @@ export async function GET({ params, request }) {
 	// TODO: we can stream the incomplete file.
 	await AwaitDownloadCompletion(chosenSearchRes.username, chosenFile.filename);
 
-	let filename_arr = chosenFile.filename.split("\\");
+	const filename_arr = chosenFile.filename.split("\\");
 	// slskd will auto-save the file in this directory format. TODO -- check for edge cases?
-	let filePath = filename_arr
+	const filePath = filename_arr
 		.slice(Math.max(filename_arr.length - 2, 0))
 		.join("/");
 
 	console.log(filePath);
 
 	// TODO: Create a cache db associating mbid to filepath
-	const readStream = fs.createReadStream(`./tests/slsk-downloads/${filePath}`);
+	const readStream = fs.createReadStream(`/home/percs/.local/share/slskd/downloads/${filePath}`);
 	const webStream = Readable.toWeb(readStream) as ReadableStream<Uint8Array>;
+	
 	return new Response(webStream, {
 		status: 200,
 		headers: {
