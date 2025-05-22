8e5eb0f39c281e9eb5cec7d0c2f70a74980da734
Trent <60309933+luphoria@users.noreply.github.com>
2025-05-22 16:05:18 -0500

    remove completed TODOs

--- tests/yt-dlp.cjs
+++ tests/yt-dlp.cjs
@@ -25,7 +25,6 @@ const SearchAndDownload = async (query) => {
 	try {
 		let resultsRaw = JSON.parse(
 			await execPromise(
-				// TODO: sanitize
 				`yt-dlp --default-search ytsearch ytsearch10:"${quote([query])} song" --no-playlist --no-check-certificate --flat-playlist --skip-download -f bestaudio --dump-single-json`
 			)
 		).entries;
@@ -86,7 +85,6 @@ const SearchAndDownload = async (query) => {
 	let filePath = (
 		await execPromise(
 			// We don't use -x because the file path doesn't properly print the new extension. Instead, let's just make sure it's the smallest/worst video file.
-			// TODO: sanitize
 			`yt-dlp "${quote([results[0].id])}" -f wv+ba -P ./tests/yt-dlp --print "after_move:filename"`
 		)
 	).split("\n")[0];
