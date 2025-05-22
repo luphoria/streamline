3793bc60fcee8b3209314002d069719118b68e5c
Trent <60309933+luphoria@users.noreply.github.com>
2025-05-21 21:14:34 -0500

    tests: functional yt-dlp search

--- tests/yt-dlp.cjs
+++ tests/yt-dlp.cjs
@@ -1,19 +1,32 @@
-const YTDlpWrap = require("yt-dlp-wrap").default;
+const { exec } = require('child_process')
+const prompt = require("prompt-sync")();
 
-(async () => {
-	const ytDlpWrap = new YTDlpWrap("yt-dlp");
+const execPromise = (input) => {
+  return new Promise((resolve, reject) => {
+    exec(input, (error, stdout, stderr) => {
+      if (error) {
+        reject(`error: ${error.message}`)
+        return
+      }
 
-	let metadata = await ytDlpWrap.getVideoInfo([
-		"--default-search",
-		"ytsearch",
-		`ytsearch10:"${"The Beatles - Oh! Darling"} song"`,
-		"--no-playlist",
-		"--no-check-certificate",
-		"--flat-playlist",
-		"--skip-download",
-	]);
+      if (stderr) {
+        resolve(stderr)
+      } else {
+        resolve(stdout)
+      }
+    })
+  })
+}
 
-	for (let res in metadata) {
-		console.log(metadata[res].title);
+const SearchAndDownload = async (query) => {
+  try {
+    const results = JSON.parse(await execPromise(`yt-dlp --default-search ytsearch ytsearch10:"${query} song" --no-playlist --no-check-certificate --flat-playlist --skip-download -f bestaudio --dump-single-json`)).entries;
+	for (let result in results) {
+		console.log(`${results[result].channel} - ${results[result].title} (${results[result].id})`);
+
 	}
-})();
+	} catch (err) {
+    console.error(err)
+  }
+}
+SearchAndDownload(prompt("Search: "));
