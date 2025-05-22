791f778aa69f94ecee68856823c42d210da67756
Trent <60309933+luphoria@users.noreply.github.com>
2025-05-22 17:16:39 -0500

    add yt-dlp.ts

---
+++ server/sources/yt-dlp.ts
@@ -0,0 +1,99 @@
+import { exec } from "child_process";
+import quote from "shell-quote/quote";
+import fs from "fs";
+
+const execPromise = (input) => {
+	return new Promise((resolve, reject) => {
+		exec(input, (error, stdout, stderr) => {
+			if (error) {
+				reject(`error: ${error.message}`);
+
+				return;
+			}
+
+			if (stderr) {
+				resolve(stderr);
+			} else {
+				resolve(stdout);
+			}
+		});
+	});
+};
+
+export const YTDLPSearchAndDownload = async (query) => {
+	// Search
+	let results: {channel: string,title: string,id: string}[] = [];
+	try {
+		let resultsRaw = JSON.parse(
+			(await execPromise(
+				`yt-dlp --default-search ytsearch ytsearch10:"${quote([query])} song" --no-playlist --no-check-certificate --flat-playlist --skip-download -f bestaudio --dump-single-json`
+			)) as string
+		).entries;
+		for (let result in resultsRaw) {
+			console.log(
+				`${resultsRaw[result].channel} - ${resultsRaw[result].title} (${resultsRaw[result].id})`
+			);
+			results.push({
+				channel: resultsRaw[result].channel,
+				title: resultsRaw[result].title,
+				id: resultsRaw[result].id,
+			});
+		}
+	} catch (err) {
+		console.error(err);
+	}
+	console.log(results);
+	console.log(`${results.length} results before filtering`);
+	// Filter results
+	query = query.toLowerCase();
+	for (let res in results)
+		results[res].title = results[res].title.toLowerCase();
+
+	if (!query.includes("remix"))
+		results = results.filter((res) => {
+			return !res.title.includes("remix");
+		});
+	if (!query.includes("edit"))
+		results = results.filter((res) => {
+			return !res.title.includes("edit");
+		});
+	if (!query.includes("live"))
+		results = results.filter((res) => {
+			return !res.title.includes("live");
+		});
+	if (!query.includes("video"))
+		results = results.filter((res) => {
+			return !res.title.includes("video");
+		});
+	if (!query.includes("full album"))
+		// probably doesn't need to be in this if, but just in case
+		results = results.filter((res) => {
+			return !res.title.includes("full album");
+		});
+	if (!query.includes("cover"))
+		results = results.filter((res) => {
+			return !res.title.includes("cover");
+		});
+
+	if (results.length == 0) return { status: 404, msg: "sorry bro" };
+
+	console.log(`${results.length} results after filtering`);
+
+	// TODO: Sort results by things like if channel author is artist
+
+	console.log(results[0]);
+
+	let filePath = (
+		(await execPromise(
+			// We don't use -x because the file path doesn't properly print the new extension. Instead, let's just make sure it's the smallest/worst video file.
+			`yt-dlp "${quote([results[0].id])}" -f wv+ba -P ./tests/yt-dlp --print "after_move:filename"`
+		)) as string
+	).split("\n")[0];
+
+	console.log(filePath);
+
+	// TODO: Create a cache db associating mbid to filepath
+	const readStream = fs.createReadStream(filePath);
+
+	return readStream;
+};
