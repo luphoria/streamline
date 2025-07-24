e9ffe3069998f0cf3b531a1894c4e3c2dcca66c7
Percs <me@percs.dev>
2025-07-23 20:52:08 -0500

    oops

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,6 +1,6 @@
 import Database from "better-sqlite3";
 
-//const db = new Database("./streamline.db");
+const db = new Database("./streamline.db");
 
 // Sqlite3 does not directly support UUIDS, so MBID is just a string
 /*
@@ -12,7 +12,7 @@ songs {
   }
 }
 */
-/*
+
 db.exec(`
     CREATE TABLE IF NOT EXISTS songs (
       mbid TEXT UNIQUE PRIMARY KEY,
@@ -20,31 +20,25 @@ db.exec(`
       source TEXT
     )
 `);
-*/
+
 
 export const AddRecording = (mbid: string, filepath: string, source?: string) => {
 	console.log(`DB: ${mbid} / ${filepath} (${source})`);
-	/*
   const insert = db.prepare(
 		"INSERT INTO songs (mbid, filepath, source) VALUES (?, ?, ?)"
 	);
 
 	return insert.run(mbid, filepath, source);
-  */
 };
 
 export const GetRecording = (mbid) => {
-  /*
 	const get = db.prepare("SELECT * FROM songs WHERE mbid = ?");
 
 	return get.get(mbid);
-  */
 };
 
 export const DeleteRecording = (mbid) => {
-  /*
 	const del = db.prepare("DELETE FROM songs WHERE mbid = ?");
 
 	return del.run(mbid);
-  */
 };
